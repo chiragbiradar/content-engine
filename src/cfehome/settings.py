fbd958eacca5350fcbb5dcb946cd1021df36186e
chiragbiradar <chiragsb16@gmail.com>
2024-03-17 00:38:30 +0530

    DB connections

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+from .env import config
 
 from pathlib import Path
 
@@ -21,10 +22,11 @@ print(os.environ.get("WELCOME_MSG"))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^(8f2wo2sflk6vxbn7j!r14fqhda4p4r@%20*)nu3rk(0bvcgm'
+SECRET_KEY = config("DJANGO_SECRET_KEY", default=None, cast=str)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG", default = False, cast = bool) 
 
 ALLOWED_HOSTS = ["*"]
 
@@ -75,13 +77,23 @@ WSGI_APPLICATION = 'cfehome.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASE_URL = config("DATABASE_URL", default = None)
+if DATABASE_URL is not None:
+    import dj_database_url
+    dj_database_url_config = dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=60,
+        conn_health_checks=True
+    )
+    DATABASES = {
+        "default": dj_database_url_config
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
